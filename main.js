016f6accb420148624ea8188c70012caea88196d
R4WEN <111735615+R4WEN@users.noreply.github.com>
2023-05-18 19:36:25 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const { mainModule } = require('process');
+
+const hostname = '127.0.0.1';
+const port = 3003;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!!! :)');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`HOLY SHIT THAT WAS SO EASY`);
+  server.close();
+});
